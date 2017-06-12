2e7be20e02d7e82e01422f2b3ca23f3d7ca495e3
Getulio Sánchez <valentin2507@gmail.com>
2017-06-12 11:54:45 -0400

    Issue #2877068 by c.nish2k3, gvso, amr.amajeed: Clicking "Not now" on facebook app throws error after redirecting

--- src/Controller/FacebookAuthController.php
+++ src/Controller/FacebookAuthController.php
@@ -9,6 +9,7 @@ use Drupal\social_auth_facebook\FacebookAuthManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\social_auth_facebook\FacebookAuthPersistentDataHandler;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Returns responses for Simple FB Connect module routes.
@@ -37,6 +38,13 @@ class FacebookAuthController extends ControllerBase {
   private $facebookManager;
 
   /**
+   * Used to access GET parameters.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  private $request;
+
+  /**
    * The Facebook Persistent Data Handler.
    *
    * @var \Drupal\social_auth_facebook\FacebookAuthPersistentDataHandler
@@ -52,17 +60,21 @@ class FacebookAuthController extends ControllerBase {
    *   Manages user login/registration.
    * @param \Drupal\social_auth_facebook\FacebookAuthManager $facebook_manager
    *   Used to manage authentication methods.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request
+   *   Used to access GET parameters.
    * @param \Drupal\social_auth_facebook\FacebookAuthPersistentDataHandler $persistent_data_handler
    *   FacebookAuthPersistentDataHandler object.
    */
-  public function __construct(NetworkManager $network_manager, SocialAuthUserManager $user_manager, FacebookAuthManager $facebook_manager, FacebookAuthPersistentDataHandler $persistent_data_handler) {
+  public function __construct(NetworkManager $network_manager, SocialAuthUserManager $user_manager, FacebookAuthManager $facebook_manager, RequestStack $request, FacebookAuthPersistentDataHandler $persistent_data_handler) {
     $this->networkManager = $network_manager;
     $this->userManager = $user_manager;
     $this->facebookManager = $facebook_manager;
+    $this->request = $request;
     $this->persistentDataHandler = $persistent_data_handler;
 
     // Sets the plugin id.
     $this->userManager->setPluginId('social_auth_facebook');
+
     // Sets the session keys to nullify if user could not logged in.
     $this->userManager->setSessionKeysToNullify([
       $this->persistentDataHandler->getSessionPrefix() . 'access_token',
@@ -77,6 +89,7 @@ class FacebookAuthController extends ControllerBase {
       $container->get('plugin.network.manager'),
       $container->get('social_auth.user_manager'),
       $container->get('social_auth_facebook.manager'),
+      $container->get('request_stack'),
       $container->get('social_auth_facebook.persistent_data_handler')
     );
   }
@@ -116,6 +129,13 @@ class FacebookAuthController extends ControllerBase {
    * Facebook returns the user here after user has authenticated in FB.
    */
   public function returnFromFb() {
+    // Checks if user cancel login via Facebook.
+    $error = $this->request->getCurrentRequest()->get('error');
+    if ($error == 'access_denied') {
+      drupal_set_message($this->t('You could not be authenticated.'), 'error');
+      return $this->redirect('user.login');
+    }
+
     /* @var \Facebook\Facebook|false $facebook */
     $facebook = $this->networkManager->createInstance('social_auth_facebook')->getSdk();
 
