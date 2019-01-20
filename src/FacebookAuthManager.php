acdaa09a891ad6830334adf0374834257eb59afc
Getulio Sánchez <valentin2507@gmail.com>
2019-01-20 18:54:52 -0500

    Issue #3027011: Simplify logic for extra scopes

--- src/FacebookAuthManager.php
+++ src/FacebookAuthManager.php
@@ -64,12 +64,7 @@ class FacebookAuthManager extends OAuth2Manager {
 
     $extra_scopes = $this->getScopes();
     if ($extra_scopes) {
-      if (strpos($extra_scopes, ',')) {
-        $scopes = array_merge($scopes, explode(',', $extra_scopes));
-      }
-      else {
-        $scopes[] = $extra_scopes;
-      }
+      $scopes = array_merge($scopes, explode(',', $extra_scopes));
     }
 
     // Returns the URL where user will be redirected.
