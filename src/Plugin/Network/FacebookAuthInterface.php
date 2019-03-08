4d6ad081205edcac64ea98788ab19873f3b81aa2
Getulio Sánchez <valentin2507@gmail.com>
2019-03-08 12:45:57 -0500

    Issue #3038663: Use Social Auth Network Base

--- src/Plugin/Network/FacebookAuthInterface.php
+++ src/Plugin/Network/FacebookAuthInterface.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\social_auth_facebook\Plugin\Network;
 
-use Drupal\social_api\Plugin\NetworkInterface;
+use Drupal\social_auth\Plugin\Network\NetworkInterface;
 
 /**
  * Defines the Facebook Auth interface.
